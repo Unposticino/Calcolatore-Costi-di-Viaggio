5dce363b6164c93cdd6547f33a14b24b22a17011
Unposticino <webapp2omar@gmail.com>
2025-09-26 21:40:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TravelCostCalculator from '././travel-cost-demo.jsx'
+import TravelCostCalculator from './travel-cost-demo.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
