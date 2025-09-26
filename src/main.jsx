960e1f27cf6d44e0f1de4a1be782f5cb6d33c90b
Unposticino <webapp2omar@gmail.com>
2025-09-26 21:14:38 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TravelCostCalculator from './TravelCostCalculator'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TravelCostCalculator />
+  </React.StrictMode>,
+)
